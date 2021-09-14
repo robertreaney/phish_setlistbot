7c80e6c4138471afbed2195ff4839acdb54f8b8f
robertreaney <63528440+robertreaney@users.noreply.github.com>
2021-09-13 21:56:16 -0500

    added date finder and data population

--- utils/getdata.py
+++ utils/getdata.py
@@ -1,18 +1,16 @@
 import requests as r
 import json
 from html.parser import HTMLParser
-import datetime
+from datetime import datetime
 from tqdm import tqdm
+import concurrent.futures as cf
 
-
-key = "B93C5925297AB520755D"
-url = f"https://api.phish.net/v3"
-
-##################################################################
-# phish.in
-in_key = '10ad07b2f96ab003b640192eb04590eb7ceb0ef4f6cb86a38308aede051d9960704857dc697326c1d49d99ed922ab6ad'
-in_url = 'http://phish.in/api/v1/'
-
+PARAMETERS = {
+    'net_key' : "B93C5925297AB520755D",
+    'net_url' : f"https://api.phish.net/v3",
+    'in_key' : '10ad07b2f96ab003b640192eb04590eb7ceb0ef4f6cb86a38308aede051d9960704857dc697326c1d49d99ed922ab6ad',
+    'in_url' : 'http://phish.in/api/v1/'
+}
 
 
 
@@ -40,45 +38,45 @@ class MyParser(HTMLParser):
             #     self.data['segue'].append(data.strip())
 
 
-
-###############################################################
-class DataManager():
-    def __init__(self, url, key):
-        self.url = url
-        self.key = key
+class ApiWrapper():
+    def __init__(self, PARAMETERS):
         self.parser = MyParser()
-        self.shows = []
+        self.data = PARAMETERS
+
+    def get_shows(self, per_page, page):
+        message = r.get(self.data['in_url'] + '/shows',
+            headers={
+                'Authorization': f'Bearer {self.data["in_key"]}',
+                'Accept': 'application/json'
+                },
+            params={
+                'sort_attr': 'date',
+                'sort_dir': 'desc',
+                'per_page': per_page,
+                'page': page
+        })
+        return message
 
     def get_eras(self):
-        response = r.get(f"{in_url}/eras", headers={
+        response = r.get(f"{self.data['in_url']}/eras", headers={
             "Accept": "application/json",
-            "Authorization": f"Bearer {in_key}"
+            "Authorization": f"Bearer {self.data['in_key']}"
         })
         self.eras = response.json()['data']
         return self.eras
+    
+###############################################################
+class DataManager(ApiWrapper):
+    def get_showdata(self, start_date):
+        self._get_showdates(start_date)
+        self.data['setlists'] = {x: self.get_setlist(x) for x in self.data['show_dates']}
 
-    def get_showdates(self, past_years):
-        years = .5
-        today = datetime.date.today()
-        start = today
-        how_long = datetime.timedelta(days=365.25*years)
-        delta = datetime.timedelta(days=1)
-
-        # TODO 5it/s there has got to be an easier way to find out all the days with shows
-        with tqdm(total=(today - (today-how_long)).days) as pbar:
-            while today > start - how_long:
-                today = today - delta
-                date = today.strftime("%Y-%m-%d")
-                if self.get_showdata(date) is not None:
-                    self.shows.append(date)
-                pbar.update(1)
-
-    def get_showdata(self, date):
+    def get_setlist(self, date):
         """Returns showdata if there is a show, returns None is there isn't
         args:
             date: str - showdate
         """
-        response = r.get(f"{self.url}/setlists/get?apikey={self.key}", \
+        response = r.get(f"{self.data['net_url']}/setlists/get?apikey={self.data['net_key']}", \
             params={
             "showdate": date
             }).json()
@@ -108,10 +106,35 @@ class DataManager():
         except:
             return None
 
+    # private
+    def _datetime(self, date):
+        """return datetime object from a date like -> '2018-11-03'
+        """
+        return datetime(*[int(x) for x in date.split('-')])
 
-dm = DataManager(url, key)
-dm.get_showdates(3)
-dm.shows
-dm.get_showdata('2018-11-03') == None
+    def _get_showdates(self, start_date):
+        """populate self.data['show_dates'] with all shows going back to supplied start_date arg."""
+        # loop through the others until we reach the end date
+        self.data['show_dates'] = []
+        message = self.get_shows(100, page=1)
+        self.data['show_dates'] = [x['date'] for x in message.json()['data'] if 
+            self._datetime(x['date']) > self._datetime(start_date)]
+        total_pages = message.json()['total_pages']
+        current_page = 2
+        with tqdm(total=total_pages, desc='Getting showdates.') as pbar:
+            pbar.update(1)
+            while current_page <= total_pages:
+                message = self.get_shows(100, page=current_page)
+                self.data['show_dates'] += [x['date'] for x in message.json()['data'] if 
+                    self._datetime(x['date']) > self._datetime(start_date)]
+                current_page += 1
+                if self._datetime(message.json()['data'][-1]['date']) < self._datetime(start_date):
+                    print("\nHit start_date, terminating early!")
+                    break
+                pbar.update(1)
 
-
+dm = DataManager(PARAMETERS)
+# dm.get_showdates('2018-11-03')
+# dm.data['show_dates']
+# dm.get_setlist('2018-11-03')
+dm.get_showdata('2009-01-01')
